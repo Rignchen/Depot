ea9f2e01df711b285fb7617fe65c7c90691e28e5
Rignchen <rinchendemort@gmail.com>
2024-07-10 18:46:11 +0200

    feat: read the `/etc/os-releas` file to get the os

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::io::Read;
+
 /// List of errors that the program can return.
 pub enum DepotError {
     UnknownOperatingSystem,
@@ -15,7 +17,17 @@ impl OperatingSystem {
     /// Get the currently running operating system.
     pub fn current() -> Result<OperatingSystem, DepotError> {
         match std::env::consts::OS {
-            "linux" => Ok(OperatingSystem::Arch),
+            "linux" => {
+                let mut file = std::fs::File::open("/etc/os-release").unwrap();
+                let mut contents = String::new();
+                // Read the file and look for the NAME field.
+                file.read_to_string(&mut contents).unwrap();
+                let os = contents
+                    .split('\n')
+                    .find(|line| line.starts_with("NAME="))
+                    .unwrap();
+                Ok(OperatingSystem::Arch)
+            }
             _ => Err(DepotError::UnknownOperatingSystem),
         }
     }
