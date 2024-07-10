a9f7fca7cee3aff80074ba84d593262534895ded
Rignchen <rinchendemort@gmail.com>
2024-07-10 18:44:07 +0200

    feat: detect if the user is on linux

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+/// List of errors that the program can return.
+pub enum DepotError {
+    UnknownOperatingSystem,
+}
+
 /// List of all supported operating systems.
 pub enum OperatingSystem {
     Arch,
@@ -5,5 +10,14 @@ pub enum OperatingSystem {
     Debian,
     Ubuntu,
     Fedora,
+}
+impl OperatingSystem {
+    /// Get the currently running operating system.
+    pub fn current() -> Result<OperatingSystem, DepotError> {
+        match std::env::consts::OS {
+            "linux" => Ok(OperatingSystem::Arch),
+            _ => Err(DepotError::UnknownOperatingSystem),
+        }
+    }
 }
 
