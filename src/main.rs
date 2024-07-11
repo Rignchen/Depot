ee63577fbee76fb7083bff2b4b0f936aadb66615
Rignchen <rinchendemort@gmail.com>
2024-07-11 09:16:15 +0200

    feat: basic subcommand handling

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,31 @@ use clap::Parser;
 ///  search     Search for a package
 ///  update     Update the package list
 #[derive(Parser)]
-struct Args {}
+struct Args {
+    #[clap(subcommand)]
+    cmd: Command,
+}
+#[derive(Parser)]
+enum Command {
+    Install(Install),
+    Remove(Remove),
+    Search(Search),
+    Update(Update),
+}
+#[derive(Parser)]
+struct Install {
+    package: String,
+}
+#[derive(Parser)]
+struct Remove {
+    package: String,
+}
+#[derive(Parser)]
+struct Search {
+    query: String,
+}
+#[derive(Parser)]
+struct Update {}
 
 /// Main function of the program
 /// Parse the command line arguments
@@ -20,5 +44,10 @@ struct Args {}
 ///  - get the os name and deduce it from there
 fn main() {
     let args = Args::parse();
-    println!("Hello, world!");
+    println!("{}", match args.cmd {
+        Command::Install(i) => format!("Install package: {}", i.package),
+        Command::Remove(r) => format!("Remove package: {}", r.package),
+        Command::Search(s) => format!("Search for package: {}", s.query),
+        Command::Update(_) => "Update the package list".to_string(),
+    });
 }
