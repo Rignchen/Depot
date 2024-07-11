d71f1ec17f6615b86b8bccdab1a6563d17e2d5be
Rignchen <rinchendemort@gmail.com>
2024-07-11 19:37:27 +0200

    feat: change bash test script to handle command line argument

--- test/test_on_all_os.sh
+++ test/test_on_all_os.sh
@@ -26,7 +26,7 @@ echo ""
 # run all docker containers and say the OS name
 for os_name in $os_names; do
     echo "test from: $os_name"
-    docker run --rm "test_on_all_os:$os_name"
+    docker run --rm "test_on_all_os:$os_name" $@
 done
 
 echo ""
