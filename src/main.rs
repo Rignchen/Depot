b4e354251e44476aa2c272635df264bd53342401
Rignchen <rinchendemort@gmail.com>
2024-07-11 09:22:17 +0200

    refactor: use structstruck to make the Args struct more readable

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,40 @@
 use clap::Parser;
 
-/// Structure of the command line arguments
-///
-/// Usage: depot [COMMAND]
-///
-/// Commands:
-///  install    Install a package
-///  remove     Remove a package
-///  search     Search for a package
-///  update     Update the package list
-#[derive(Parser)]
-struct Args {
-    #[clap(subcommand)]
-    cmd: Command,
+structstruck::strike! {
+    /// Structure of the command line arguments
+    ///
+    /// Usage: depot [COMMAND]
+    ///
+    /// Commands:
+    ///  install    Install a package
+    ///  remove     Remove a package
+    ///  search     Search for a package
+    ///  update     Update the package list
+    #[strikethrough[derive(Parser)]]
+    struct Args {
+        #[clap(subcommand)]
+        cmd: enum Command {
+            Install(
+                struct Install {
+                    package: String,
+                }
+            ),
+            Remove(
+                struct Remove {
+                    package: String,
+                }
+            ),
+            Search(
+                struct Search {
+                    query: String,
+                }
+            ),
+            Update(
+                struct Update {}
+            ),
+        }
+    }
 }
-#[derive(Parser)]
-enum Command {
-    Install(Install),
-    Remove(Remove),
-    Search(Search),
-    Update(Update),
-}
-#[derive(Parser)]
-struct Install {
-    package: String,
-}
-#[derive(Parser)]
-struct Remove {
-    package: String,
-}
-#[derive(Parser)]
-struct Search {
-    query: String,
-}
-#[derive(Parser)]
-struct Update {}
 
 /// Main function of the program
 /// Parse the command line arguments
