fe01f213cd2cafa98c135c7092e166787880a4ca
Rignchen <rinchendemort@gmail.com>
2024-07-11 09:15:22 +0200

    feat: install clap to handle command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+
+/// Structure of the command line arguments
+///
+/// Usage: depot [COMMAND]
+///
+/// Commands:
+///  install    Install a package
+///  remove     Remove a package
+///  search     Search for a package
+///  update     Update the package list
+#[derive(Parser)]
+struct Args {}
+
+/// Main function of the program
+/// Parse the command line arguments
+/// Look for the package manager in
+///  - the command line arguments
+///  - the environment variables
+///  - get the os name and deduce it from there
 fn main() {
+    let args = Args::parse();
     println!("Hello, world!");
 }
