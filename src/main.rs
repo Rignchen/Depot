9d334fb13a53d0253e5a810e0e56f28f981a8c12
Nils Hofstetter <119439839+Rignchen@users.noreply.github.com>
2024-07-11 19:48:14 +0200

    feat: handle command line arguments (#7)
    feat: basic subcommand handling
    
    refactor: use structstruck to make the Args struct more readable
    
    feat: add the ability to manualy specify the package manager
    
    feat: change bash test script to handle command line argument

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,48 @@
+use clap::Parser;
 use depot::{unwrap_depot_error, OperatingSystem};
+
+structstruck::strike! {
+    /// Structure of the command line arguments
+    ///
+    /// Usage: depot [OPTION] <COMMAND>
+    ///
+    /// Options:
+    ///  -p, --pm, --package-manager <package-manager>   Manually select the package manager
+    ///
+    /// Commands:
+    ///  install    Install a package
+    ///  remove     Remove a package
+    ///  search     Search for a package
+    ///  update     Update the package
+    #[strikethrough[derive(Parser, Debug)]]
+    struct Args {
+        #[clap(short, long, alias = "pm")]
+        package_manager: Option<String>,
+        #[clap(subcommand)]
+        cmd: enum Command {
+            Install(
+                struct Install {
+                    package: String,
+                }
+            ),
+            Remove(
+                struct Remove {
+                    package: String,
+                }
+            ),
+            Search(
+                struct Search {
+                    query: String,
+                }
+            ),
+            Update(
+                struct Update {
+                    package: Option<String>,
+                }
+            ),
+        }
+    }
+}
 
 /// Main function of the program
 /// Parse the command line arguments
@@ -7,6 +51,20 @@ use depot::{unwrap_depot_error, OperatingSystem};
 ///  - the environment variables
 ///  - get the os name and deduce it from there
 fn main() {
+    let args = Args::parse();
+    println!("Args: {:?}", args);
+    println!(
+        "{}",
+        match args.cmd {
+            Command::Install(i) => format!("Install package: {}", i.package),
+            Command::Remove(r) => format!("Remove package: {}", r.package),
+            Command::Search(s) => format!("Search for package: {}", s.query),
+            Command::Update(u) => format!(
+                "Update package list: {}",
+                u.package.unwrap_or("all".to_string())
+            ),
+        }
+    );
     let os = unwrap_depot_error(OperatingSystem::current());
     println!("OS: {:?}", os);
 }
