c27a1b7e47f0ed82d3820a3be89deb1cac4c4349
Rignchen <rinchendemort@gmail.com>
2024-07-14 19:09:12 +0200

    feat: add aliases for commands

--- src/cli.rs
+++ src/cli.rs
@@ -10,31 +10,35 @@ structstruck::strike! {
     ///  -p, --pm, --package-manager <package-manager>   Manually select the package manager
     ///
     /// Commands:
-    ///  install    Install a package
-    ///  remove     Remove a package
-    ///  search     Search for a package
-    ///  update     Update the package
+    ///  install i    Install a package
+    ///  remove  r    Remove a package
+    ///  search  s    Search for a package
+    ///  update  u    Update the package
     #[strikethrough[derive(Parser, Debug)]]
     pub struct Args {
         #[clap(short, long, alias = "pm", value_enum)]
         pub package_manager: Option<PackageManager>,
         #[clap(subcommand)]
         pub cmd: pub enum Command {
+            #[clap(alias = "i")]
             Install(
                 struct Install {
                     pub package: String,
                 }
             ),
+            #[clap(alias = "r")]
             Remove(
                 struct Remove {
                     pub package: String,
                 }
             ),
+            #[clap(alias = "s")]
             Search(
                 struct Search {
                     pub query: String,
                 }
             ),
+            #[clap(alias = "u")]
             Update(
                 struct Update {
                     pub package: Option<String>,
